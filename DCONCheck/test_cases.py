7c021c9f041dbea7b541e2b27a68dcd2b8902feb
Jonathan <jho@pocketgems.com>
2014-07-15 16:23:05 -0700

    updating submodule

--- DCONCheck/test_cases.py
+++ DCONCheck/test_cases.py
@@ -8,24 +8,26 @@ from tools import *
 
 errors = {}
 dcon_repo_dir_txt = open('../info.txt', 'r')
-dcon_staging_dir = dcon_repo_dir_txt.read() + 'staging/'
+dcon_staging_dir = dcon_repo_dir_txt.read() + '/staging/'
 all_csvs = getAllCSV(dcon_staging_dir)
+csvs = csv_class_factory(dcon_staging_dir)
+print csvs["Achievements.csv"].row('LTQsuperhero1')
 
 #********************************* List of Test Cases ************************************
 def tests():
-	dupeAchievementStepsIdCheck()
+	# dupeAchievementStepsIdCheck()
 	return errors
 
 #********************************* EXAMPLE: Dupe Achievement Steps ID Check ***********************
-def dupeAchievementStepsIdCheck():
-	test_name = 'Duplicate Achievements Steps ID Check'
-	achievement_steps_csv = all_csvs['Achievement.csv']
-	ids = []
-	for row in achievement_steps_csv:
-		ids.append(row[0])
-	id_occurances = Counter(ids)
-	dupe_ids = {}
-	for id in id_occurances:
-		if id_occurances[id] > 1:
-			dupe_ids[id] = id_occurances[id]
-	errors[test_name] = dupe_ids
+# def dupeAchievementStepsIdCheck():
+# 	test_name = 'Duplicate Achievements Steps ID Check'
+# 	achievement_steps_csv = all_csvs['Achievement.csv']
+# 	ids = []
+# 	for row in achievement_steps_csv:
+# 		ids.append(row[0])
+# 	id_occurances = Counter(ids)
+# 	dupe_ids = {}
+# 	for id in id_occurances:
+# 		if id_occurances[id] > 1:
+# 			dupe_ids[id] = id_occurances[id]
+# 	errors[test_name] = dupe_ids
